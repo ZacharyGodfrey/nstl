de3a4a5f4aee10250b5d38d898551155e37401b6
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-11-17 11:34:53 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   seasons: [
-    // {
-    //   title: 'Season 3: November 2023',
-    //   docId: '1XQmRx9HF06-f_zDRLoFQk0N49Mj2Bv7HSs5r9bUnuv4',
-    //   sheetName: 'Leaderboard'
-    // },
+    {
+      title: 'Season 3: November 2023',
+      docId: '1XQmRx9HF06-f_zDRLoFQk0N49Mj2Bv7HSs5r9bUnuv4',
+      sheetName: 'Leaderboard'
+    },
     {
       title: 'Season 2: September 2023',
       docId: '10m2s8m3kyIc2GPB87y9hhoVHTBqUsmZwiy0OxuyIGoQ',
